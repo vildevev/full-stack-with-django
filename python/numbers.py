abbee1f1330dbf542a1e3b8ddca36401aa1d263d
“Vilde <vilde@vevatne.no>
2017-09-22 14:02:13 -0700

    Add file for numbers in python

---
+++ python/numbers.py
@@ -0,0 +1,13 @@
+# Python is a dynamic programming language. Don't have to declare what type a variable is going to be.  
+a = 5 
+print(a+a)
+# support reassignment
+a = a*5
+print(a)
+# variable names can't start with a number or symbol ex. "2dogs = "tom""
+# snakecase is convention ex. "hello_there"
+
+my_income = 100 #integer
+tax_rate = 0.1 # floating-point number
+my_taxes = my_income * tax_rate # res = floating-point number
+print(my_taxes)
