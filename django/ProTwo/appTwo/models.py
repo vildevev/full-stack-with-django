69eb719d1877ef7a9136b46b48ba9d86ef41160a
“Vilde <vilde@vevatne.no>
2017-10-26 09:50:21 -0700

    Create simple project with users

---
+++ django/ProTwo/appTwo/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class User(models.Model):
+		first_name = models.CharField(max_length=128)
+		last_name = models.CharField(max_length=128)
+		email = models.EmailField(max_length=264, unique=True)
