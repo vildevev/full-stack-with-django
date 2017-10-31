2eefa21eb6f441330137cdacc413d9ea3054a8d7
“Vilde <vilde@vevatne.no>
2017-10-31 14:52:02 -0700

    Add email validator

--- django/basicforms/basicapp/forms.py
+++ django/basicforms/basicapp/forms.py
@@ -4,9 +4,13 @@ from django.core import validators
 class FormName(forms.Form):
 		name = forms.CharField()
 		email = forms.EmailField()
+		verify_email = forms.EmailField(label='Enter your email again:')
 		text = forms.CharField(widget=forms.Textarea)
-		botcatcher = forms.CharField(required=False,
-																 widget=forms.HiddenInput,
-																 validators=[validators.MaxLengthValidator(0)])
 
-		
+		def clean(self):
+			all_clean_data = super().clean()
+			email = all_clean_data['email']
+			vmail = all_clean_data['verify_email']
+
+			if email != vmail:
+					raise forms.ValidationError("Make sure emails match")
